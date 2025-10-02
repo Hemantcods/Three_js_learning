ebbcd6b00dc42503a81e8182581985daaa260c04
Hemantcods <hy564636@gmail.com>
2025-10-02 12:26:08 +0530

    Refactor main.js: Add OrbitControls, enable wireframe for mesh, and improve formatting

--- Vite/Three/src/main.js
+++ Vite/Three/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // scene
 let scene = new THREE.Scene();
 
@@ -9,33 +10,38 @@ scene.add(camera);
 
 // mesh(geometry and material)
 let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: "white" });
+let material = new THREE.MeshBasicMaterial({ color: "white", wireframe: true });
 let mesh = new THREE.Mesh(geometry, material);
 
-mesh.position.x =0;
-mesh.position.y =0;
-mesh.position.z =0;
+mesh.position.x = 0;
+mesh.position.y = 0;
+mesh.position.z = 0;
 scene.add(mesh);
 
 // renderer
 const canvas = document.querySelector("canvas");
-let renderer = new THREE.WebGLRenderer({ canvas,antialias: true });
+let renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
-window.addEventListener("resize", () =>{
+window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 })
 
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// clock
 const clock = new THREE.Clock();
 // animation
 function animate() {
-  requestAnimationFrame(animate);
-  mesh.rotation.x = clock.getElapsedTime() * 0.5;
-
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    mesh.rotation.x = clock.getElapsedTime() * 0.5;
+    controls.update();
+    renderer.render(scene, camera);
 }
 animate();
